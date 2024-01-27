15d96df11d7db3a6b013c579b477f81e7cbe40d3
miaa <belmouaddine.ptech@gmail.com>
2024-01-27 15:57:52 +0100

    adding the quiz service,authinterceptor, working on displaying quizzes

--- src/app/Components/Quizzes/Display-quiz/display-quiz.component.ts
+++ src/app/Components/Quizzes/Display-quiz/display-quiz.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { QuizService } from 'src/app/Services/Quiz/quiz.service';
+import { Quiz } from 'src/app/Models/quiz.model';
 
 @Component({
   selector: 'app-display-quiz',
   templateUrl: './display-quiz.component.html',
-  styleUrls: ['./display-quiz.component.css']
+  styleUrls: ['./display-quiz.component.css'],
 })
-export class DisplayQuizComponent {
+export class DisplayQuizComponent implements OnInit {
+  quizId!: number ;
+  quiz : Quiz = new Quiz;
 
+  constructor(private route: ActivatedRoute, private quizService: QuizService) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.quizId = params['id'];
+      this.getQuizById();
+    });
+  }
+
+  getQuizById() {
+    this.quizService.getQuizById(this.quizId).subscribe(
+      (quiz: Quiz) => {
+        this.quiz = quiz;
+        console.log(quiz)
+      },
+      (error) => {
+        console.error('Error fetching quiz by ID:', error);
+      }
+    );
+  }
 }
