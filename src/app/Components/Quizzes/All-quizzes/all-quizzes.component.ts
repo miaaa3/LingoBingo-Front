ebebe56b7e7adf3cf023b3906b1c7f728bcb72b0
hicham <hichamghoutani52@gmail.com>
2024-02-05 15:04:35 +0100

    add some features

--- src/app/Components/Quizzes/All-quizzes/all-quizzes.component.ts
+++ src/app/Components/Quizzes/All-quizzes/all-quizzes.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { getQuizCategories } from 'src/app/Models/enums/category.enum';
+import { Quiz } from 'src/app/Models/quiz.model';
+import { QuizService } from 'src/app/Services/Quiz/quiz.service';
 
 @Component({
   selector: 'app-all-quizzes',
   templateUrl: './all-quizzes.component.html',
   styleUrls: ['./all-quizzes.component.css']
 })
-export class AllQuizzesComponent {
+export class AllQuizzesComponent implements OnInit{
   quizCategories: string[];
+  quizzes: Quiz[] = [];
+  isLoading:boolean = true;
 
-  constructor() {
+  constructor(
+    private quizService: QuizService,
+    private router:Router
+      ) {
     this.quizCategories = getQuizCategories();
   }
+  async ngOnInit(): Promise<void> {
+    this.quizzes = await this.quizService.getAllQuizzes().toPromise();
+    this.isLoading = false;
+  }
+
+  navigateToQuiz(){
+    this.router.navigate(['/Display-quiz/:id'])
+  }
+
+  
 }
